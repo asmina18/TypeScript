afb4c72cfa029d18fee6ec8a21e71e4c49873e23
asmina18 <asmina18@mail.ru>
2024-01-09 10:35:02 +0100

    arrays er færdig

---
+++ built/arrays.js
@@ -0,0 +1,11 @@
+"use strict";
+// Array med strings
+let stringArray = ["abc", "def", "ghi"];
+// Array med tal
+let numberArray = [1, 2, 3, 4, 5];
+// Mixed array
+let mixedArray = ["abc", 123, true, "def", 456];
+
+console.log(stringArray); // ["abc", "def", "ghi"]
+console.log(numberArray); // [1, 2, 3, 4, 5]
+console.log(mixedArray); // ["abc", 123, true, "def", 456]
---
+++ src/arrays.ts
@@ -0,0 +1,13 @@
+// Array med strings
+let stringArray: string[] = ["abc", "def", "ghi"];
+
+// Array med tal
+let numberArray: number[] = [1, 2, 3, 4, 5];
+
+// Mixed array
+let mixedArray: (string | number | boolean)[] = ["abc", 123, true, "def", 456];
+
+
+console.log(stringArray); // ["abc", "def", "ghi"]
+console.log(numberArray); // [1, 2, 3, 4, 5]
+console.log(mixedArray);  // ["abc", 123, true, "def", 456]
